12a43215761d4c8cba908e608f46910c60ec8f84
NicoHood <git@nicohood.de>
2020-09-05 21:07:15 +0200

    Added getDistance() function

--- classes/geocoding.php
+++ classes/geocoding.php
@@ -70,4 +70,32 @@ class Geocoding
             return null;
         }
     }
+
+    /**
+    * Calculates the great-circle distance between two points, with
+    * the Vincenty formula.
+    * https://stackoverflow.com/questions/10053358
+    * @param float $latitudeFrom Latitude of start point in [deg decimal]
+    * @param float $longitudeFrom Longitude of start point in [deg decimal]
+    * @param float $latitudeTo Latitude of target point in [deg decimal]
+    * @param float $longitudeTo Longitude of target point in [deg decimal]
+    * @param float $earthRadius Mean earth radius in [m]
+    * @return float Distance between points in [m] (same as earthRadius)
+    */
+    public static function getDistance($latitudeFrom, $longitudeFrom, $latitudeTo, $longitudeTo, $earthRadius = 6371000)
+    {
+        // convert from degrees to radians
+        $latFrom = deg2rad($latitudeFrom);
+        $lonFrom = deg2rad($longitudeFrom);
+        $latTo = deg2rad($latitudeTo);
+        $lonTo = deg2rad($longitudeTo);
+
+        $lonDelta = $lonTo - $lonFrom;
+        $a = pow(cos($latTo) * sin($lonDelta), 2) +
+          pow(cos($latFrom) * sin($latTo) - sin($latFrom) * cos($latTo) * cos($lonDelta), 2);
+        $b = sin($latFrom) * sin($latTo) + cos($latFrom) * cos($latTo) * cos($lonDelta);
+
+        $angle = atan2(sqrt($a), $b);
+        return $angle * $earthRadius;
+    }
 }
